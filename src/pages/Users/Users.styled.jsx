b5c390ab0247e2b578ddda239c8df80e53870f6b
Ihor Statsenko <statsenkoin@gmail.com>
2023-04-23 01:12:48 +0300

    Create Users.styled.jsx

---
+++ src/pages/Users/Users.styled.jsx
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const PageWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  max-width: 800px;
+  margin-left: auto;
+  margin-right: auto;
+  padding-inline: 24px;
+`;
+
+export const PageHead = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  padding: 12px 24px;
+  border-bottom: 1px solid #888;
+`;
