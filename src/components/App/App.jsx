a10cb0375df1183e73bb9e767e08875089cc35e8
Ihor Statsenko <statsenkoin@gmail.com>
2023-04-23 01:14:11 +0300

    Rename route

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,14 +3,14 @@ import { Routes, Route } from 'react-router-dom';
 import { Layout } from 'components';
 
 const Home = lazy(() => import('../../pages/Home/Home'));
-const Tweets = lazy(() => import('../../pages/Tweets/Tweets'));
+const Users = lazy(() => import('../../pages/Users/Users'));
 
 export function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
-        <Route path="tweets" element={<Tweets />} />
+        <Route path="users" element={<Users />} />
         <Route path="*" element={<Home />} />
       </Route>
     </Routes>
