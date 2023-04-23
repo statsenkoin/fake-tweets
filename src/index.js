047d67f1e90447ecd4cc6b304bcb7a3b397c363d
Ihor Statsenko <statsenkoin@gmail.com>
2023-04-23 20:41:32 +0300

    import default Montserrat font

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from 'components';
 import './index.css';
+import '@fontsource/montserrat/500.css';
+import '@fontsource/montserrat/600.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
