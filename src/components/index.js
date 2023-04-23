e32ab473d04885dc136e90bf40f1c21ea5651fa2
Ihor Statsenko <statsenkoin@gmail.com>
2023-04-23 17:39:20 +0300

    add UserCard reexport

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export { Layout } from './Layout/Layout';
 export { LoadingPage } from './LoadingPage/LoadingPage';
 export { Footer } from './Footer/Footer';
 export { BackLink } from './BackLink/BackLink';
+export { UserCard } from './UserCard/UserCard';
