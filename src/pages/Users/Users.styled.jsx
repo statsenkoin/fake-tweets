1e61f0f15141ba02f3def0a66a8c75d5c8b8fcd7
Ihor Statsenko <statsenkoin@gmail.com>
2023-04-23 20:03:04 +0300

    add UsersList

--- src/pages/Users/Users.styled.jsx
+++ src/pages/Users/Users.styled.jsx
@@ -20,3 +20,10 @@ export const PageHead = styled.div`
   margin-bottom: 24px;
   border-bottom: 1px solid #888;
 `;
+
+export const UsersList = styled.ul`
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+  margin-bottom: 24px;
+`;
