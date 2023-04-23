7414ce08f73fb9b20628b961c7eaea5d411a4bae
Ihor Statsenko <statsenkoin@gmail.com>
2023-04-23 20:03:47 +0300

    add test users array and map it

--- src/pages/Users/Users.jsx
+++ src/pages/Users/Users.jsx
@@ -1,6 +1,46 @@
 import { useLocation } from 'react-router-dom';
-import { PageWrapper, PageHead } from './Users.styled';
-import { BackLink } from 'components';
+import { PageWrapper, PageHead, UsersList } from './Users.styled';
+import { BackLink, UserCard } from 'components';
+
+// import hansel from '../../assets/images/Hansel.png';
+const users = [
+  {
+    user: 'Elon Mask',
+    tweets: 7777,
+    followers: 100500,
+    // avatar: '',
+    avatar: require('../../assets/images/Hansel.png'),
+    id: '1',
+  },
+  {
+    user: 'Keanu Reeves',
+    tweets: 555,
+    followers: 800300,
+    avatar: require('../../assets/images/Hansel.png'),
+    id: '2',
+  },
+  {
+    user: 'Joe Biden',
+    tweets: 333,
+    followers: 1000001,
+    avatar: require('../../assets/images/Hansel.png'),
+    id: '3',
+  },
+  {
+    user: 'Bill Gates',
+    tweets: 1000,
+    followers: 7777,
+    avatar: require('../../assets/images/Hansel.png'),
+    id: '4',
+  },
+  {
+    user: 'Mark Zuckerberg',
+    tweets: 999,
+    followers: 3003,
+    avatar: '',
+    id: '5',
+  },
+];
 
 export default function Users() {
   const location = useLocation();
@@ -12,7 +52,13 @@ export default function Users() {
           <BackLink to={backLinkHref}>Back to Home</BackLink>
           <span>filter</span>
         </PageHead>
-        <h3>Users</h3>
+        <UsersList>
+          {users.map(user => (
+            <li key={user.id}>
+              <UserCard user={user}></UserCard>
+            </li>
+          ))}
+        </UsersList>
       </PageWrapper>
     </main>
   );
