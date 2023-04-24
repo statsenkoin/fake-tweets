8f7b54671abc85f5335412562c4f0fa322c45a21
Ihor Statsenko <statsenkoin@gmail.com>
2023-04-24 19:32:31 +0300

    add updateUser query

--- src/services/usersApi.js
+++ src/services/usersApi.js
@@ -1,10 +1,14 @@
 import axios from 'axios';
 
 axios.defaults.baseURL = 'https://6421e46c86992901b2be8a71.mockapi.io/users';
-axios.defaults.params = { limit: 3 };
+// axios.defaults.params = { limit: 3 };
+const perPage = 3;
 
 export async function fetchUsers(page) {
-  const response = await axios.get(`?page=${page}`);
-  console.log('response :>> ', response);
+  const response = await axios.get(`?page=${page}&limit=${perPage}`);
   return response.data;
+}
+
+export async function updateUser({ id, followers }) {
+  await axios.put(`/${id}`, { followers });
 }
