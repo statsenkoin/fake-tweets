1275ccce4a6ff16a315c80a40faae281f9abe732
Ihor Statsenko <statsenkoin@gmail.com>
2023-04-24 17:03:47 +0300

    add btn onClick logic

--- src/components/UserCard/UserCard.jsx
+++ src/components/UserCard/UserCard.jsx
@@ -6,14 +6,25 @@ import {
   AvatarFrame,
   AvatarBg,
   Meta,
-  FollowBtn,
+  // FollowBtn,
 } from './UserCard.styled';
+import { FollowBtn } from 'components/Button/Button.styled';
 
 import bgImage from '../../assets/images/bgPicture.png';
 import logo from '../../assets/images/logoGoIT.png';
+import { useState } from 'react';
 
-export function UserCard({ user }) {
+export function UserCard({ user, followingUsers, handleFollowingButton }) {
+  const isFollowingUser = followingUsers.some(({ id }) => user.id === id);
+  const [btnActive, setBtnActive] = useState(isFollowingUser);
+
   const { tweets, followers, avatar, user: userName } = user;
+
+  const handleBtnActive = () => {
+    setBtnActive(!btnActive);
+    handleFollowingButton(user);
+  };
+
   return (
     <Card>
       <BgImage src={bgImage} alt="background_picture" />
@@ -21,18 +32,18 @@ export function UserCard({ user }) {
       <Divider />
       <AvatarFrame>
         <AvatarBg>
-          <img
-            width="62px"
-            height="62px"
-            // src={require('../../assets/images/Hansel.png')}
-            src={avatar}
-            alt={userName}
-          />
+          <img width="62px" height="62px" src={avatar} alt={userName} />
         </AvatarBg>
       </AvatarFrame>
       <Meta>{tweets.toLocaleString('en-US')} tweets</Meta>
       <Meta>{followers.toLocaleString('en-US')} followers</Meta>
-      <FollowBtn type="button">follow</FollowBtn>
+      <FollowBtn
+        type="button"
+        $mode={btnActive ? 'active' : ''}
+        onClick={handleBtnActive}
+      >
+        {btnActive ? 'following' : 'follow'}
+      </FollowBtn>
     </Card>
   );
 }
