8247c6fc043c028dd7e617efbca93548ca8b5f35
Ihor Statsenko <statsenkoin@gmail.com>
2023-04-25 15:40:58 +0300

    add text, styles

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,15 +1,29 @@
 import { Link, useLocation } from 'react-router-dom';
-import { PageWrapper } from './Home.styled';
+import { PageWrapper, Text } from './Home.styled';
 
 export default function Home() {
   const location = useLocation();
   return (
-    <main>
-      <PageWrapper>
-        <Link to="/users" state={{ from: location }}>
-          Home
-        </Link>
-      </PageWrapper>
-    </main>
+    <PageWrapper>
+      <Text>
+        This small SPA (single page application) presents a simple connection to
+        the{' '}
+        <span>
+          <a href="https://mockapi.io/projects" target="blank">
+            mocapi.io
+          </a>
+        </span>{' '}
+        server to display a list of "fake tweets" users, perform some simple
+        operations, and navigate the page.
+      </Text>
+      <Text>
+        More in "Footer" and on the{' '}
+        <span>
+          <Link to="/users" state={{ from: location }}>
+            User Page
+          </Link>
+        </span>
+      </Text>
+    </PageWrapper>
   );
 }
