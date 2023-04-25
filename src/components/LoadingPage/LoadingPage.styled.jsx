369d39cdcd9f573ac36d2666b358d860530c0f54
Ihor Statsenko <statsenkoin@gmail.com>
2023-04-25 15:39:58 +0300

    Update LoadingPage.styled.jsx

--- src/components/LoadingPage/LoadingPage.styled.jsx
+++ src/components/LoadingPage/LoadingPage.styled.jsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const LoadingMessage = styled.p`
   text-align: center;
-  font-size: 48px;
-  font-weight: 700;
+  font-size: 36px;
+  font-weight: 500;
   line-height: 3em;
 `;
