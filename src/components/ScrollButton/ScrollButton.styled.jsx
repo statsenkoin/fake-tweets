61ba89b34fd0fa6a2000de2a723e763f663ef227
Ihor Statsenko <statsenkoin@gmail.com>
2023-04-25 22:13:25 +0300

    Create ScrollButton.styled.jsx

---
+++ src/components/ScrollButton/ScrollButton.styled.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Button = styled.div`
+  position: fixed;
+  width: 100%;
+  left: 85%;
+  bottom: 100px;
+  height: 20px;
+  font-size: 3rem;
+  z-index: 1;
+  cursor: pointer;
+  color: var(--color-bg-light-btn-active);
+`;
